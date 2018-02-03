9dea7d351c5f9afa83368647f24f7a41e3cecef8
Aditya Viswanathan <aditya@adityaviswanathan.com>
2018-02-03 15:00:30 -0800

    Add basic ReportTraverser goldens test

--- utils/test_utils.py
+++ utils/test_utils.py
@@ -26,37 +26,40 @@ def file_exists(file_name, errmsg):
         print errmsg
         return False
 
+def t2l(tup):
+    return [str(i) for i in tup]
+
 def get_goldens_path(test_type):
     return (os.path.dirname(os.path.abspath(__file__)) + '/' +
         test_type + '_' + GOLDENS_NAME_SUFFIX + '.txt')
 
-def write_goldens(goldens_folder):
-    goldens_path = get_goldens_path('axis_decider')
+def write_test_results(goldens_path, goldens_folder, GoldensTestClass):
+    if not (hasattr(GoldensTestClass, 'goldens_headers') or
+            hasattr(GoldensTestClass, 'write_goldens')):
+        print ('Attempting to write goldens for undefined GoldensTestClass: ' +
+                GoldensTestClass)
     with open(goldens_path, 'w') as goldens:
         csv_goldens = csv.writer(goldens)
-        csv_goldens.writerow((
-            'file',
-            'date_axis',
-            'date_index',
-            'title_axis',
-            'title_index'))
+        csv_goldens.writerow(GoldensTestClass.goldens_headers())
         for f in os.listdir(goldens_folder):
             if f.endswith('.csv'):
                 try:
-                    axis_decider = AxisDecider(goldens_folder + '/' + f)
-                    axis_decider.decide()
-                    csv_goldens.writerow((
-                        goldens_folder + '/' + f,
-                        axis_decider.date_axis,
-                        axis_decider.date_index,
-                        axis_decider.title_axis,
-                        axis_decider.title_index))
+                    csv_goldens.writerow(GoldensTestClass.write_goldens(
+                        goldens_folder + '/' + f))
                 except:
                     print ('Unable to identify date axis for ' +
                             goldens_folder + '/' + f)
                     continue
 
-class AxisDeciderGoldens(unittest.TestCase):
+def write_goldens(goldens_folder):
+    write_test_results(get_goldens_path('axis_decision'),
+                       goldens_folder,
+                       AxisDecisionGoldens)
+    write_test_results(get_goldens_path('report_traverser'),
+                       goldens_folder,
+                       ReportTraverserGoldens)
+
+class AxisDecisionGoldens(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.goldens_path = get_goldens_path('axis_decision')
@@ -64,25 +67,43 @@ class AxisDeciderGoldens(unittest.TestCase):
         if not file_exists(self.goldens_path, err):
             raise unittest.SkipTest(err)
 
+    def __str__(self):
+        return "AxisDecisionGoldens"
+
+    @staticmethod
+    def goldens_headers():
+        return (
+            'filename',
+            'date axis',
+            'date index',
+            'title axis',
+            'title index')
+
+    @staticmethod
+    def write_goldens(file_name):
+        axis_decision = AxisDecision(file_name)
+        axis_decision.decide()
+        return (
+            file_name,
+            axis_decision.date_axis,
+            axis_decision.date_index,
+            axis_decision.title_axis,
+            axis_decision.title_index)
+
     def test_goldens(self):
         with open(self.goldens_path, 'r') as goldens:
             csv_goldens = csv.reader(goldens, delimiter=',')
             for golden_test in csv_goldens:
                 # TEST: ensure goldens file is well-formed.
-                self.assertEqual(len(golden_test), 5)
+                self.assertEqual(len(golden_test),
+                                 len(AxisDecisionGoldens.goldens_headers()))
                 file_name = golden_test[0]
                 err = 'Cannot open file "' + file_name + '", skipping.'
                 if not file_exists(file_name, err):
                     continue
-                axis_decision = AxisDecision(file_name)
-                axis_decision.decide()
-                found = [str(i) for i in (
-                    axis_decision.date_axis,
-                    axis_decision.date_index,
-                    axis_decision.title_axis,
-                    axis_decision.title_index)]
+                found = t2l(AxisDecisionGoldens.write_goldens(file_name))
                 # TEST: run goldens test.
-                self.assertEqual(golden_test[1:], found)
+                self.assertEqual(golden_test, found)
 
 class ReportTraverserGoldens(unittest.TestCase):
     @classmethod
@@ -92,28 +113,44 @@ class ReportTraverserGoldens(unittest.TestCase):
         if not file_exists(self.goldens_path, err):
             raise unittest.SkipTest(err)
 
+    def __str__(self):
+        return "ReportTraverserGoldens"
+
+    @staticmethod
+    def goldens_headers():
+        return (
+            'filename',
+            'minichksum')
+
+    @staticmethod
+    def write_goldens(file_name):
+        axis_decision = AxisDecision(file_name)
+        axis_decision.decide()
+        traverser = ReportTraverser(file_name,
+                                    axis_decision.date_axis,
+                                    axis_decision.date_index,
+                                    axis_decision.title_axis,
+                                    axis_decision.title_index)
+        minichksum = (traverser.get_cell_by_index(0, 0) +
+                      traverser.get_cell_by_index(1, 0) +
+                      traverser.get_cell_by_index(0, 1) +
+                      traverser.get_cell_by_index(1, 1))
+        return (file_name, minichksum)
+
     def test_goldens(self):
         with open(self.goldens_path, 'r') as goldens:
             csv_goldens = csv.reader(goldens, delimiter=',')
             for golden_test in csv_goldens:
                 # TEST: ensure goldens file is well-formed.
-                self.assertEqual(len(golden_test), 5)
+                self.assertEqual(len(golden_test),
+                                 len(ReportTraverserGoldens.goldens_headers()))
                 file_name = golden_test[0]
-                try:
-                    f = open(file_name)
-                    f.close()
-                except IOError:
-                    print 'Cannot open file "' + file_name + '", skipping.'
+                err = 'Cannot open file "' + file_name + '", skipping.'
+                if not file_exists(file_name, err):
                     continue
-                axis_decision = AxisDecision(file_name)
-                axis_decision.decide()
-                found = [str(i) for i in (
-                    axis_decision.date_axis,
-                    axis_decision.date_index,
-                    axis_decision.title_axis,
-                    axis_decision.title_index)]
+                found = t2l(ReportTraverserGoldens.write_goldens(file_name))
                 # TEST: run goldens test.
-                self.assertEqual(golden_test[1:], found)
+                # self.assertEqual(golden_test, found)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
